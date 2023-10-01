ba65b79757a6fb6bdcc5e24560f4ca4cb2897217
denervalspelizario <denervalspelizario@gmail.com>
2023-10-01 10:22:52 -0300

    update adicao de flatlist que renderiza na screen groups todos os card de groupcard e se não houver renderiza component listempty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,13 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import { HighLight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);  // state tipado como array de string
+
   return (
     <Container>
       <Header />
@@ -11,10 +16,25 @@ export function Groups() {
         title='Turmas'  // repasssando os dados de props
         subTitle='jogue com sua turma'
       />  
-      <GroupCard 
-        title='Team MIBR'
       
+      <FlatList
+        data={groups} // dados inicias state groups
+        
+        keyExtractor={item => item} // como key de referencia seré o item
+        
+        renderItem={({item}) => (
+          <GroupCard title={item} //renderizar(o que vai mostrar) será o component GroupCard que recebe como props os dados de item(os dados da state group)
+        />)}
+        
+        contentContainerStyle={groups.length == 0 && {flex: 1}} // se não tiver dados na state group estiliza com flex 1
+        ListEmptyComponent={() => ( // component que será renderizado somente quando NÂO tiver nada para renderizar na state groups
+          <ListEmpty message="Que tal cadastrar uma turma?"/>
+        )}
       />
+
+      
+      
+
     </Container>
   );
 }
